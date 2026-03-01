405daed67ba9d3bd0c88fde9900278ddb3b290b3
attu0 <atharvmudse@gmail.com>
2026-03-01 16:40:02 +0530

    added launch file for real robot

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -3,39 +3,47 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
-import xacro
 
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('articubot_two'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file).toxml()
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    pkg_path = os.path.join(
+        get_package_share_directory('articubot_two')
+    )
+
+    xacro_file = os.path.join(
+        pkg_path,
+        'description',
+        'robot.urdf.xacro'
+    )
+
+    robot_description = Command([
+        'xacro ',
+        xacro_file,
+        ' sim_mode:=',
+        use_sim_time
+    ])
+
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': use_sim_time
+        }]
     )
 
-
-    # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='true',
-            description='Use sim time if true'),
-
+            description='Use sim time if true'
+        ),
         node_robot_state_publisher
     ])
