c27ae87bc54fe08145822344c3b86e01c330db33
attu0 <atharvmudse@gmail.com>
2026-06-26 16:07:13 +0530

    Working configuration for actual robot

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='rplidar_ros',
-            executable='rplidar_node',
+            executable='rplidar_composition',
             name='rplidar',
             output='screen',
             parameters=[{
