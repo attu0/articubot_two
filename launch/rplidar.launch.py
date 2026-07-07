f0da86b7318ac496c22044f024f8c5781dbb9973
attu0 <atharvmudse@gmail.com>
2026-07-07 23:04:02 +0530

    Added a direct bash launch script to launch sim

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -5,15 +5,15 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='rplidar_ros',
-            executable='rplidar_node',
+            executable='rplidar_composition',
             name='rplidar',
             output='screen',
             parameters=[{
                 'serial_port': '/dev/ttyUSB0',
                 'serial_baudrate': 115200,
                 'frame_id': 'laser_frame',
-                'angle_compensate': False,
-                'scan_mode': 'Standard'
+                'angle_compensate': True,
+                'scan_mode': 'Express',
             }]
         )
     ])
